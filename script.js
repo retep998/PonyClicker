8954f5e88dc6e57a3ab2ffecd8ffebab8fe49738
Peter Atashian <retep998@gmail.com>
2014-02-24 23:53:36 -0500

    Now saves pony count in localStorage.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+
 count = 0;
-function init() {
-    document.getElementById("pony").addEventListener("click", click_pony, false);
+
+function savePony() {
+    localStorage.setItem("count", count);
 }
-function click_pony() {
-    ++count;
+function loadPony() {
+    count = Number(localStorage.getItem("count"));
+    updatePony();
+}
+function init() {
+    loadPony();
+    document.getElementById("pony").addEventListener("click", clickPony, false);
+    window.addEventListener("unload", savePony, false);
+    setInterval(savePony(), 1000 * 60);
+}
+function updatePony() {
     document.getElementById("count").textContent = count;
+}
+function clickPony() {
+    ++count;
+    updatePony();
 }
