07682ceb13b9724cc5d5ea5033834dfd0e30d9e2
Peter Atashian <retep998@gmail.com>
2014-02-25 00:39:34 -0500

    Added boops.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- script.js
+++ script.js
@@ -17,7 +17,28 @@ function init() {
 function updatePony() {
     document.getElementById("count").textContent = count;
 }
-function clickPony() {
+function clickPony(e) {
     ++count;
+    createBoop(e.clientX, e.clientY);
     updatePony();
+}
+function createBoop(x, y) {
+    var boop = document.createElement("p");
+    boop.appendChild(document.createTextNode("*boop*"));
+    boop.setAttribute("class", "boop");
+    boop.style.opacity = 1;
+    boop.style.position = "absolute";
+    boop.style.left = x + "px";
+    boop.style.top = y + "px";
+    document.getElementById("effects").appendChild(boop);
+    setTimeout(updateBoop, 100, boop);
+}
+function updateBoop(boop) {
+    var x = Number(boop.style.opacity) - 0.1;
+    if (x < 0.1) {
+        document.getElementById("effects").removeChild(boop);
+    } else {
+        boop.style.opacity = x;
+        setTimeout(updateBoop, 100, boop);
+    }
 }
