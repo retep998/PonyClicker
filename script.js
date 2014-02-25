c1ce0b1e290e75efc8e48e3cfb841a1e9868951d
Peter Atashian <retep998@gmail.com>
2014-02-25 03:30:22 -0500

    Experimenting with layouts.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var pony = {
         pony.updateCount();
     },
     updateCount: function () {
-        document.getElementById("count").textContent = pony.formatNumber(pony.count);
+        document.getElementById("count").textContent = pony.formatNumber(pony.count) + " boops";
         var n;
         if (pony.count < 10) {
             n = 1;
@@ -35,14 +35,13 @@ var pony = {
     },
     createBoop: function (x, y) {
         var boop = document.createElement("p");
-        var pos = document.getElementById("ponyarea");
         boop.appendChild(document.createTextNode("*boop*"));
         boop.setAttribute("class", "boop");
         boop.style.opacity = 1;
         boop.style.position = "absolute";
-        boop.style.left = x - 32 - pos.getBoundingClientRect().left + "px";
-        boop.style.top = y - 16 - pos.getBoundingClientRect().top + "px";
-        document.getElementById("boops").appendChild(boop);
+        boop.style.left = x - 32 + "px";
+        boop.style.top = y - 16 + "px";
+        document.getElementById("effects").appendChild(boop);
         setTimeout(pony.updateBoop, 50, boop);
         new Audio("boop.mp3").play();
     },
@@ -54,7 +53,7 @@ var pony = {
     updateBoop: function (boop) {
         var x = Number(boop.style.opacity);
         if (x < 0.1) {
-            document.getElementById("boops").removeChild(boop);
+            document.getElementById("effects").removeChild(boop);
         } else {
             boop.style.opacity = x - 1. / 16;
             boop.style.top = Number(boop.style.top.substr(0, boop.style.top.indexOf("px"))) - 1 + "px";
