9fcdd8e0a2f6ca981a62dd5e99fae9cfad8b2dcf
Mees Brinkhuis <MeesGamez@gmail.com>
2014-10-26 12:57:55 +0100

    added changes that have been sitting idle on my server for way to long

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 
+var interval = 0;
+
+function startBps() {
+    if(interval > 0) clearInterval(interval);
+    interval = setInterval("bps()", 1000);
+}
+
 var pony = {
     count: 0,
     muted: false,
+    perClick: 1,
+    perSecond: 0,
     formatNumber: function (x) {
         var parts = x.toString().split(".");
         parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -10,13 +19,19 @@ var pony = {
     save: function () {
         localStorage.setItem("count", pony.count);
         localStorage.setItem("muted", pony.muted);
+        localStorage.setItem("perClick", pony.perClick);
+        localStorage.setItem("perSecond", pony.perSecond);
     },
     load: function () {
         pony.count = Number(localStorage.getItem("count"));
         pony.muted = localStorage.getItem("muted") == "true";
+        pony.perClick = Number(localStorage.getItem("perClick"));
+	if(pony.perClick <= 0)
+	    pony.perClick = 1;
+        pony.perSecond = Number(localStorage.getItem("perSecond"));
     },
     update: function () {
-        document.getElementById("count").textContent = pony.formatNumber(pony.count) + " boops";
+        document.getElementById("count").textContent = Math.round(pony.count) + " boops";
         var n;
         if (pony.count < 10) {
             n = 1;
@@ -24,11 +39,15 @@ var pony = {
             n = 2;
         } else if (pony.count < 1000) {
             n = 3;
-        } else {
-            n = 4;
+        } else if (pony.count < 5000) {
+			n = 4;	
+		} else {
+            n = 5;
         }
         document.getElementById("ponyimg").setAttribute("src", "assets/img/pony" + n + ".png");
         document.getElementById("mute").textContent = pony.muted ? "Unmute" : "Mute";
+        document.getElementById("bpc").innerHTML = "boops per click: " + pony.perClick;
+        document.getElementById("bps").innerHTML = "boops per second: " + pony.perSecond.toFixed(1);
     },
     init: function () {
         pony.load();
@@ -40,12 +59,15 @@ var pony = {
         document.getElementById("reset").addEventListener("click", function () {
             if (confirm("Are you sure you want to reset your progress?")) {
                 pony.count = 0;
+                pony.perClick = 1;
+                pony.perSecond = 0;
                 pony.update();
             }
         });
         window.addEventListener("unload", pony.save, false);
         setInterval(pony.save, 1000 * 60);
         pony.update();
+        startBps();
     },
     createBoop: function (x, y) {
         var boop = document.createElement("p");
@@ -66,7 +88,7 @@ var pony = {
         }
     },
     click: function (e) {
-        pony.count++;
+        pony.count += pony.perClick;
         pony.createBoop(e.clientX, e.clientY);
         pony.update();
     },
@@ -80,4 +102,41 @@ var pony = {
             setTimeout(pony.updateBoop, 50, boop);
         }
     }
+}
+
+function added(init, cost){
+	switch(init){
+		case 1:
+		if(pony.count >= cost){ 
+			pony.count -= cost;
+			pony.perClick++;
+		}
+		break;
+		case 2:
+		if(pony.count >= cost){ 
+			pony.count -= cost;
+			pony.perSecond += 0.5;
+		}
+		break;
+		case 3:
+                if(pony.count >= cost){
+                        pony.count -= cost;
+                        pony.perSecond *= 1.01;
+                }
+                break;
+                case 4:
+                if(pony.count >= cost){
+                        pony.count -= cost;
+                        pony.perClick += 10;
+                }
+                break;
+		default:
+		break;
+	}
+	pony.update();
+}
+
+function bps(){
+    pony.count += pony.perSecond;
+    pony.update();
 }
